3a5861867b706d02f63667e5733f9ff322bc18f4
lepioo <pitikev1@hotmail.com>
2018-06-30 02:13:53 +0200

    add characters

--- TM1637.js
+++ TM1637.js
@@ -9,7 +9,76 @@ wpi.setup("gpio");
 //  E |   | C
 //     ---
 //      D
-var allowedChars=["0","1","2","3","4","5","6","7","8","9","a","b","c","d","e","f","h","i","j","l","o","p","q","s","t","u","y"];
+var allowedChars=['0',
+'1',
+'2',
+'3',
+'4',
+'5',
+'6',
+'7',
+'8',
+'9',
+'A',
+'a',
+'B',
+'b',
+'C',
+'c',
+'D',
+'d',
+'E',
+'e',
+'F',
+'f',
+'G',
+'g',
+'H',
+'h',
+'I',
+'i',
+'J',
+'j',
+'K',
+'k',
+'L',
+'l',
+'M',
+'m',
+'N',
+'n',
+'O',
+'o',
+'P',
+'p',
+'Q',
+'q',
+'R',
+'r',
+'S',
+'s',
+'T',
+'t',
+'U',
+'u',
+'V',
+'v',
+'W',
+'w',
+'X',
+'x',
+'Y',
+'y',
+'Z',
+'z',
+' ',
+'°',
+'-',
+'=',
+'[',
+']',
+'(',
+')'];
 codigitToSegment = [
     // XGFEDCBA
     0b00111111, // 0
@@ -23,22 +92,65 @@ codigitToSegment = [
     0b01111111, // 8
     0b01101111, // 9
     0b01110111, // A
+    0b01011111, // a
+    0b01111111, // B
     0b01111100, // b
     0b00111001, // C
+    0b01011000, // c
+    0b00111111, // D
     0b01011110, // d
     0b01111001, // E
+    0b01111011, // e
     0b01110001, // F
+    0b01110001, // f
+    0b00111101, // G
+    0b01101111, // g
     0b01110110, // H
+    0b01110100, // h
     0b00000110, // I
+    0b00000100, // i
     0b00011110, // J
+    0b00001110, // j
+    0b01110110, // K
+    0b01110100, // k
     0b00111000, // L
+    0b00000110, // l
+    0b00110111, // M
+    0b01010100, // m
+    0b01110110, // N
+    0b01010100, // n
+    0b00111111, // O
     0b01011100, // o
     0b01110011, // P
+    0b01110011, // p
+    0b01100111, // Q
     0b01100111, // q
+    0b01110111, // R
+    0b01010000, // r
     0b01101101, // S
+    0b01101101, // s
+    0b01111000, // T
     0b01111000, // t
     0b00111110, // U
-    0b01100110 //y
+    0b00011100, // u
+    0b00111110, // V
+    0b00011100, // v
+    0b00111110, // W
+    0b00011100, // w
+    0b01110110, // X
+    0b01110110, // x
+    0b01100110, // Y
+    0b01100110, // y
+    0b01011011, // z
+    0b01011011, // Z
+    0b00000000, // " "
+    0b01100011, // °
+    0b01000000, // -
+    0b01001000, // =
+    0b00111001, // (
+    0b00001111, // )
+    0b00111001, // [
+    0b00001111  // ]
 ];
 
 const sleep = () => new Promise((r) => setTimeout(r, 1));
@@ -50,14 +162,10 @@ module.exports = class TM1637Display {
         this.pinDIO = pinDIO;
         this.trueValue = trueValue;
 
-        // 默认高电位
         wpi.pinMode(this.pinClk, wpi.OUTPUT);
         wpi.pinMode(this.pinDIO, wpi.OUTPUT);
         wpi.digitalWrite(this.pinClk, this.trueValue);
         wpi.digitalWrite(this.pinDIO, this.trueValue);
-        // this.high(this.pinClk);
-        // this.high(this.pinDIO);
-
     }
 
      high(pin) {
@@ -70,19 +178,12 @@ module.exports = class TM1637Display {
          sleep();
     }
 
-    // clock high in, high out
      start() {
-        // pinDIO  high -> low when clock is high
-        // this.high(this.pinDIO);
-        // this.high(this.pinClk);
          this.low(this.pinDIO);
     }
 
-    // clock high in, high out
      writeBit(value) {
-        // 一个上升沿
          this.low(this.pinClk);
-        // change the value when clock is low
         if (value)
              this.high(this.pinDIO);
         else
@@ -91,22 +192,16 @@ module.exports = class TM1637Display {
          this.high(this.pinClk);
     }
      readAck() {
-        // 8号下降沿
          this.low(this.pinClk);
         wpi.pinMode(this.pinDIO, wpi.INPUT);
-        // 9号上升沿
          this.high(this.pinClk);
         const ack = wpi.digitalRead(this.pinDIO);
-        // if(ack === 0)  scucces, low
         wpi.pinMode(this.pinDIO, wpi.OUTPUT);
-        // 9号下降沿
          this.low(this.pinClk);
-        // console.log(ack);
         return ack;
     }
 
-    // clock high in, low out
-     writeByte(byte) { // 0b00000000
+     writeByte(byte) {
         let b = byte;
         for (let i = 0; i < 8; i++) {
              this.writeBit(b & 0x01);
@@ -115,9 +210,7 @@ module.exports = class TM1637Display {
         return  this.readAck();
 }
 
-    // clock low in, high out
      stop() {
-        // pinDIO  low -> high  when clock is high
          this.low(this.pinDIO);
          this.high(this.pinClk);
          this.high(this.pinDIO);
@@ -136,21 +229,21 @@ module.exports = class TM1637Display {
 
      sendData(nums, split = false, cb=null) {
         let numsEncoded = [0, 0, 0, 0].map((u, i) => codigitToSegment[nums[i]] || 0);
-        if (split) numsEncoded[1] = numsEncoded[1] | 0b10000000; // the x of 2nd pos
+        if (split) numsEncoded[1] = numsEncoded[1] | 0b10000000;
 
-         this.start(); // 数据命令设置
-         this.writeByte(0b01000000); // 普通模式, 自动地址增加, 写数据到显示寄存器
+         this.start();
+         this.writeByte(0b01000000);
          this.stop();
 
-         this.start(); // 地址命令设置
-         this.writeByte(0b11000000); // 地址起始位 从0开始
+         this.start();
+         this.writeByte(0b11000000);
         for (let i = 0; i < numsEncoded.length; i++) {
              this.writeByte(numsEncoded[i]);
         }
          this.stop();
 
-         this.start(); // 地址命令设置
-         this.writeByte(0b10001111); // 显示控制命令设置, 开, 亮度为 111
+         this.start();
+         this.writeByte(0b10001111); 
          this.stop();
         if(cb)cb();
     }
